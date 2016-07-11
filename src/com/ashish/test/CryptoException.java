345bb32908b167fa1ed975fd59ed2ae5a2f6ac06
Ashishkumar Gupta <ashishkumar.gupta@amadeus.com>
2016-07-11 14:47:48 +0530

    Code for encrypting and decrypting

---
+++ src/com/ashish/test/CryptoException.java
@@ -0,0 +1,13 @@
+package com.ashish.test;
+
+public class CryptoException extends Exception {
+
+	private static final long serialVersionUID = -2993202618450328296L;
+
+	public CryptoException() {
+	}
+
+	public CryptoException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
---
+++ src/com/ashish/test/CryptoUtilsTest.java
@@ -0,0 +1,27 @@
+package com.ashish.test;
+
+import java.io.File;
+
+/**
+ * @author www.codejava.net
+ *
+ */
+public class CryptoUtilsTest {
+	public static void main(String[] args) {
+		
+		String key = "Mary has one cat";
+		File inputFile = new File("D:/Ashish/workspace/ashworkspace/SwingProjects/src/com/ashish/test/document.txt");
+		File encryptedFile = new File("D:/Ashish/workspace/ashworkspace/SwingProjects/src/com/ashish/test/document.encrypted");
+		File decryptedFile = new File("D:/Ashish/workspace/ashworkspace/SwingProjects/src/com/ashish/test/document.decrypted");
+
+		try {
+			CryptoUtils.encrypt(key, inputFile, encryptedFile);
+			System.out.println("----------- Encryption Done -----------");
+			CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+			System.out.println("----------- Decryption Done -----------");
+		} catch (CryptoException ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+}
